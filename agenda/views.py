dfad4c5e740cc21b88bc19687bc266fdc98b333e
Rafael Augusto Ramos <august.rafael@gmail.com>
2024-11-29 10:47:26 -0300

    finalização da inserção de agendamento e inicio da atualização

--- agenda/views.py
+++ agenda/views.py
@@ -1,25 +1,40 @@
 from django.shortcuts import render
 from django.views import View
+from django.core import serializers
+from django.http import HttpResponse, JsonResponse
 from .models import Agendamento
+from perfil.models import PerfilUsuario
+from . import agenda_service
 
 class Principal(View):
     
     def get(self, *args, **kwargs):
         
         context = {
-            'agendamentos' : Agendamento.objects.all()
+            'agendamentos' : Agendamento.objects.all(),
+            'perfis' : PerfilUsuario.objects.all() 
         }
 
         return render(self.request, 'agenda/agenda.html', context)
     
     
     def post(self, *args, **kwargs):
+        hora_inicio = self.request.POST.get('hora_inicio')
+        hora_final = self.request.POST.get('hora_final')
+        data_evento = self.request.POST.get('data_evento')  
+        ultima_atualizacao = self.request.POST.get('ultima_atualizacao')  
+        profissional = self.request.user
         
-        quantidade = self.request.POST.get('quantidade')
-        preco_final = self.request.POST.get('quantidade')
-        id_variacao = self.request.POST.get('id_variacao')
-        user = self.request.user
-        ProdutoService().salvar_entrada_produto(id_variacao, preco_final, quantidade, user)
+        nova_agenda = agenda_service.Agenda_Service().adiciona_evento(hora_inicio, hora_final, data_evento, profissional)
+        lista_agendas = [nova_agenda]
+        json_data = serializers.serialize('json', lista_agendas)
+        return JsonResponse(json_data, safe=False)
 
-        return redirect('produto:tabela')
-
+class Atualiza_Cliente(View):
+    
+     def post(self, *args, **kwargs):
+        perfil_id = self.request.POST.get('perfil_id');
+        evento_id = self.request.POST.get('evento_id');
+        agendamento = Agendamento.objects.filter(id=evento_id)
+        json_data = serializers.serialize('json', agendamento)
+        return JsonResponse(json_data, safe=False)
