500679bba1e2d49c6d87fb6a25b7291df39673ae
Rafael Augusto Ramos <august.rafael@gmail.com>
2024-11-29 10:40:54 -0300

    nova classe agenda_service

---
+++ agenda/agenda_service.py
@@ -0,0 +1,24 @@
+from .models import Agendamento
+from datetime import datetime
+
+class Agenda_Service():
+
+    def __new__(cls):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(Agenda_Service, cls).__new__(cls)
+        return cls.instance    
+
+    def adiciona_evento(self, dt_inicio, dt_final, data_evento, profissional):
+        
+        data_evento = datetime.strptime(data_evento, '%Y-%m-%d')
+        hora_inicio = datetime.strptime(dt_inicio, '%H:%M:%S')
+        hora_final = datetime.strptime(dt_final, '%H:%M:%S')
+
+        agenda = Agendamento(pessoa=None,
+                            profissional=profissional, 
+                            data_evento=data_evento, 
+                            hora_inicio=hora_inicio, 
+                            hora_final=hora_final)
+        agenda.save()
+
+        return agenda
