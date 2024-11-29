c2c8688126859d030cbadddcb725a63d51fbf00e
Rafael Augusto Ramos <august.rafael@gmail.com>
2024-11-29 10:44:53 -0300

    novos campos pessoa e data_evento

--- agenda/models.py
+++ agenda/models.py
@@ -4,10 +4,10 @@ from datetime import datetime
 from django.utils import timezone
 
 class Agendamento(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
-    profissional = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
-    data = models.DateField(default=datetime.now().date())
+    pessoa = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name='user_pessoa')
+    profissional = models.ForeignKey(User, on_delete=models.CASCADE, null=False, related_name='user_profissional')
+    data_evento = models.DateField(default=datetime.now().date())
     hora_inicio = models.TimeField(default=timezone.now().time())
     hora_final = models.TimeField(default=timezone.now().time())
-    observacao = models.TextField(max_length=500)
+    observacao = models.TextField(max_length=500, null=True)
     desativado = models.BooleanField(default=False)
