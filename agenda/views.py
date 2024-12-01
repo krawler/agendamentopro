6bf8db741077043f6e596a5c4634abf34c9a1123
Rafael Augusto Ramos <august.rafael@gmail.com>
2024-12-01 03:48:26 -0300

    muitas alterações

--- agenda/views.py
+++ agenda/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from django.core import serializers
 from django.http import HttpResponse, JsonResponse
@@ -7,12 +7,29 @@ from perfil.models import PerfilUsuario
 from . import agenda_service
 from datetime import datetime
 
-class Principal(View):
+
+class DispachLoginRequired(View):
+    
+    def dispatch(self, *args, **kwargs):
+        if not self.request.user.is_authenticated:
+            return redirect("perfil:login")
+    
+        return super().dispatch(*args, **kwargs)
+    
+    def get_query_set(self, *args, **kwargs):
+        qs = super().get_queryset(*args, **kwargs)
+        qs = qs.filter(usuario=self.request.user)
+        return qs
+
+
+class Principal(DispachLoginRequired, View):
     
     def get(self, *args, **kwargs):
-        
+
+        agendamentos = Agendamento.objects.exclude(json_evento='criado fora do calendario agendador')    
+        agendamentos = serializers.serialize('json', agendamentos)
         context = {
-            'agendamentos' : Agendamento.objects.filter(data_evento=datetime.today()),
+            'agendamentos' : agendamentos,
             'perfis' : PerfilUsuario.objects.all() 
         }
 
@@ -25,13 +42,15 @@ class Principal(View):
         data_evento = self.request.POST.get('data_evento')  
         ultima_atualizacao = self.request.POST.get('ultima_atualizacao')  
         profissional = self.request.user
-        
-        nova_agenda = agenda_service.Agenda_Service().adiciona_evento(hora_inicio, hora_final, data_evento, profissional)
+        json_event = self.request.POST.get('jsonEvent')
+        print('json event ' + json_event)
+        nova_agenda = agenda_service.Agenda_Service().adiciona_evento(hora_inicio, hora_final, data_evento, profissional, json_event)
         lista_agendas = [nova_agenda]
         json_data = serializers.serialize('json', lista_agendas)
+
         return JsonResponse(json_data, safe=False)
 
-class Atualiza_Cliente(View):
+class Atualiza_Cliente(DispachLoginRequired, View):
     
      def post(self, *args, **kwargs):
         perfil_id = self.request.POST.get('perfil_id');
