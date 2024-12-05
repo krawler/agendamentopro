a172aecdf9fba627ed149462f1c0bea796a6fd47
Rafael Augusto Ramos <august.rafael@gmail.com>
2024-12-04 23:56:40 -0300

    novos metodos atualiza_cliente e atualiza_json

--- agenda/agenda_service.py
+++ agenda/agenda_service.py
@@ -8,7 +8,8 @@ class Agenda_Service():
             cls.instance = super(Agenda_Service, cls).__new__(cls)
         return cls.instance    
 
-    def adiciona_evento(self, dt_inicio, dt_final, data_evento, profissional, json_event):
+
+    def adiciona_evento(self, dt_inicio, dt_final, data_evento, profissional, json_event, id_jsondiv_evento):
         
         data_evento = datetime.strptime(data_evento, '%Y-%m-%d')
         hora_inicio = datetime.strptime(dt_inicio, '%H:%M:%S')
@@ -19,7 +20,33 @@ class Agenda_Service():
                             data_evento=data_evento, 
                             hora_inicio=hora_inicio, 
                             hora_final=hora_final,
-                            json_evento=json_event)
+                            json_evento=json_event,
+                            id_jsondiv_evento=id_jsondiv_evento)
         agenda.save()
 
         return agenda
+
+
+    def atualiza_cliente(self, perfil_id, evento_id):
+
+        qs_agendamento = Agendamento.objects.filter(id=evento_id)
+        agendamento = qs_agendamento.first()
+        user_perfil_pessoa = PerfilUsuario.objects.filter(id=perfil_id).first()
+        agendamento.pessoa = user_perfil_pessoa.usuario
+        agendamento.save()
+
+
+    def atualiza_json(self, id_json_evento, json_evento):
+        
+        qs_agendamento = Agendamento.objects.filter(id_jsondiv_evento=id_json_evento)
+        agendamento = qs_agendamento.first()
+        if agendamento is not None:
+            if agendamento.json_evento == json_evento:
+                return '{return false}'
+            else:
+                agendamento.json_evento = json_evento
+                agendamento =  agendamento.save()
+            
+            return agendamento
+        
+        
