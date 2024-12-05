c1365bca149915217e65424234bf5fc0a13d5441
Rafael Augusto Ramos <august.rafael@gmail.com>
2024-12-04 23:57:31 -0300

    novo metodo atualiza json

--- agenda/urls.py
+++ agenda/urls.py
@@ -6,4 +6,5 @@ app_name = 'agenda'
 urlpatterns = [
     path('', views.Principal.as_view(), name="principal"),
     path('atualizacliente', views.Atualiza_Cliente.as_view(), name="atualizacliente"),
+    path('atualizajson', views.Atualiza_Json.as_view(), name="atualizajson"),
 ]
