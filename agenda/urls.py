428bee092f9de85f52a3598088bc446eb3acaadd
Rafael Augusto Ramos <august.rafael@gmail.com>
2024-12-05 18:06:45 -0300

    nova rota atualiza evento

--- agenda/urls.py
+++ agenda/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.Principal.as_view(), name="principal"),
     path('atualizacliente', views.Atualiza_Cliente.as_view(), name="atualizacliente"),
     path('atualizajson', views.Atualiza_Json.as_view(), name="atualizajson"),
+    path('atualizaevento', views.Atualiza_Evento.as_view(), name="atualizaevento"),
 ]
