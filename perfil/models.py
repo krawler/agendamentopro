86c916aef27cc701be4a0916004b52cf60723ca7
Rafael Augusto Ramos <august.rafael@gmail.com>
2025-01-17 17:04:31 -0300

    novos modelos PasswordResetCode e ListaDesejoProduto

--- perfil/models.py
+++ perfil/models.py
@@ -1,3 +1,4 @@
+from produto.models import Produto
 from django.db import models
 from django.contrib.auth.models import User
 from django.forms import ValidationError
@@ -8,9 +9,8 @@ from utils.validacpf import valida_cpf
 class PerfilUsuario(models.Model):
 
     usuario = models.OneToOneField(User, on_delete=models.CASCADE)
-    idade = models.PositiveIntegerField()
-    data_nascimento = models.DateField(null=True, blank=True)
-    cpf = models.CharField(max_length=14)
+    nome_completo = models.CharField(null=True, max_length=100) 
+    cpf = models.CharField(max_length=14,null=True, blank=True)
     endereco = models.CharField(null=False, max_length=100)
     numero = models.CharField(max_length=15)
     complemento = models.CharField(max_length=50, null=True, blank=True)
@@ -50,24 +50,27 @@ class PerfilUsuario(models.Model):
             ('TO', 'Tocantins'),
         )
     )
-
+    telefone = models.CharField(null=True, max_length=50)
+    perfil_endereco = models.BooleanField(default=True)
+    
     def __str__(self) :
         return f'{self.usuario}'
 
     def clean(self):
         error_messages = {}
 
-        self.cpf = self.cpf.replace('-','').replace('.','')
-        
-        if not valida_cpf(self.cpf):
-            error_messages['cpf'] = 'Digite um CPF válido'
-        
-        if re.search(r'[^0-9]', self.cpf):
-            error_messages = 'CPF inválido, digite os 11 digitos do CPF'
-
-        if error_messages:
-            raise ValidationError(error_messages)
-
     class Meta:
         verbose_name = 'Perfil'
         verbose_name_plural = 'Perfis'
+
+class PasswordResetCode(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    codigo = models.CharField(max_length=50, unique=True)
+    criado_em = models.DateTimeField(auto_now_add=True)
+    token = models.CharField(max_length=50, unique=False)
+
+class ListaDesejoProduto(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    produto = models.ForeignKey(Produto, on_delete=models.SET_NULL, null=True)
+    adicionado_em = models.DateTimeField(auto_now_add=True)
+    desativado = models.BooleanField(default=False)
