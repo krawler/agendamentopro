5bc3250552dff9015695cc57254f480b9c03e1b9
Rafael Augusto Ramos <august.rafael@gmail.com>
2025-01-29 22:27:06 -0300

    inclusao das classes admin de agendamento e configuração

--- agenda/admin.py
+++ agenda/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Agendamento, Configuracao
 
-# Register your models here.
+class AgendamentoAdmin(admin.ModelAdmin):
+    display_fields = ['pessoa', 'profissional', 'titulo', 'data_evento', 'hora_inicio', 'hora_final']
+
+class ConfiguracaoAdmin(admin.ModelAdmin):
+    display_fields = ['url_atualiza_json', 'tempo_duracao_evento']
+
+admin.site.register(Agendamento, AgendamentoAdmin)
+admin.site.register(Configuracao, ConfiguracaoAdmin)
